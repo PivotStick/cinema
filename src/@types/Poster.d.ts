6108b33fbe0f7b1fab231193105b77e6a9aedee1
Maxime Di Natale <maxime@oddiris.com>
2021-10-24 15:57:31 +0200

    deploy

--- src/@types/Poster.d.ts
+++
@@ -1,6 +0,0 @@
-export type Poster = {
-  _id: number;
-  title: string;
-  location: string;
-  format: string;
-};
--- src/@types/TabsContext.d.ts
+++
@@ -1,7 +0,0 @@
-import type { Writable } from "svelte/store";
-
-export type TabsContext = {
-  set: (name: string) => void;
-  remove: (name: string) => void;
-  current: Writable<string>;
-};
